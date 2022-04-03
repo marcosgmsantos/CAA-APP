b74fd7bd69a039d69db7e798d9e9cb8c339ba643
marcosgmsantos <102864873+marcosgmsantos@users.noreply.github.com>
2022-04-03 18:19:35 +0500

    Added UI Assets

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+
+
+
+$("#add_user").submit(function(event){
+    alert("Data Inserted Successfully!");
+})
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value']
+    })
+
+
+    var request = {
+        "url" : `http://localhost:3030/user/update/${data.username}`,
+        "method" : "put",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(response){
+        window.location.replace('/')
+    })
+
+})
+
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var username = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3030/user/delete/${username}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully!");
+                location.reload();
+            })
+        }
+
+    })
+}

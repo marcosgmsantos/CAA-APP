a486c480c95c60835cf32d499e569d5f006a574b
marcosgmsantos <102864873+marcosgmsantos@users.noreply.github.com>
2022-04-03 18:20:18 +0500

    Adding mongodb model and config

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb://imtiazmongodbusername:imtiazmongodbpassword@cluster0-shard-00-00.dcbp5.mongodb.net:27017,cluster0-shard-00-01.dcbp5.mongodb.net:27017,cluster0-shard-00-02.dcbp5.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+};

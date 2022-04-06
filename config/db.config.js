be61cf46b4bbe94f49a6f9473a33da3af75c00cd
marcosgmsantos <102864873+marcosgmsantos@users.noreply.github.com>
2022-04-07 01:04:06 +0500

    update connectionstring

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://imtiazmongodbusername:imtiazmongodbpassword@cluster0-shard-00-00.dcbp5.mongodb.net:27017,cluster0-shard-00-01.dcbp5.mongodb.net:27017,cluster0-shard-00-02.dcbp5.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+  url: "mongodb+srv://mydbuser:mydbpassword@cluster0.jcx83.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 };
